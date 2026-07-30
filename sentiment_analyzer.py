be269feff51c9a72312da2499bc079955e717615
Tnighthawk <tnighthawk51@gmail.com>
2026-07-30 08:18:36 -0700

    FIrst steps
    Finished Main Function

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -7,16 +7,9 @@ def run_sentiment_analysis():
     # CALL Watson NLP endpoint/library with text
     print("Watson has been called")
     # PARSE response to extract:
-    score = watson_response
         # negative score
-        if score == "negative":
-            print("negative")
         # neutral score
-        elif score == "neutral":
-            print("neutral")
         # positive score
-        elif score == "positive":
-            print("positive")
         # dominant sentiment label
 
         # RETURN dictionary/object with parsed values
--- server.py
+++ server.py
@@ -5,13 +5,37 @@
 # Import Flask, render_template, request from the flask pramework package : 
 # Import the sentiment_analyzer function from the package created:
 
-import flask, render_template
-from sentiment_analyzer import sentiment_analyzer
+import flask, render_template, request
+from sentiment_analyzer import sentiment_analysis
 
 #Initiate the flask app :
-app = Flask('sentiment_analyzer')
+app = Flask(__name__)
 
-@app.route("/sentimentAnalyzer")
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+session(app)
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Forget any user_id
+    session.clear()
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    user_id = session["user_id"]
+    return render_template("index.html")
+
+@app.route("/sentimentAnalyzer", methods=["GET"])
 def sent_analyzer():
     ''' This code receives the text from the HTML interface and 
         runs sentiment analysis over it using sentiment_analysis()
@@ -19,25 +43,26 @@ def sent_analyzer():
         score for the provided text.
     '''
     # GET query parameter textToAnalyze
-    # CALL sentiment_analyzer(textToAnalyze)
+    if request.method == "GET":
+        # CALL sentiment_analyzer(textToAnalyze)
+        sentiment_analysis
     # IF response contains error:
+    
     # RETURN user-friendly error message string
     # ELSE:
+    else:
+        print("error")
     # BUILD formatted sentence:
     # “For the given statement, the system response is
+
     # 'label': X,
     # 'negative': A,
     # 'neutral': B,
     # 'positive': C.”
     # RETURN formatted string to browser
 
-@app.route("/")
-def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
-    return render(index.html)
 
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
+    '''
+    app.run(debug=True, host="0.0.0.0", port=5000)
